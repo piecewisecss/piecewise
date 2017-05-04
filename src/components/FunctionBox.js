0f9934cb9511aa411fac76b93663655011f4fc64
John Fiorilla <jpfiorilla@gmail.com>
2017-05-04 11:30:13 -0400

    fixed calculation, bolding

--- src/components/FunctionBox.js
+++ src/components/FunctionBox.js
@@ -9,16 +9,16 @@ export default class FunctionBox extends React.Component{
   }
   render(){
     // console.log('state', this.state);
-    let { width } = this.state, calc = `24px + ((${width}px - 480px) / (1200px - 480px)) / (48px - 24px) = ` + (24 + (width - 480) / (1200 - 480) / (48 - 24)).toString().split('.')[0] + 'px';
+    let { width } = this.state, calc = `24px + ((${width}px - 480px) / (1200px - 480px)) / (48px - 24px) = ` + (24 + (width - 480) / ((1200 - 480) / (48 - 24))).toString().split('.')[0] + '.' + (24 + (width - 480) / ((1200 - 480) / (48 - 24))).toString().split('.')[1].substring(0, 2) + ((24 + (width - 480) / ((1200 - 480) / (48 - 24))).toString().split('.')[1].substring(0, 2).length == 1 ? '0' : '') + 'px';
     return (
       <div className='functionBox'>
         <div className='currentWidth'>{width}</div>
         <div className='calc'>
           <div className='calcLeft'>f(x) =</div>
           <div className='calcRight'>
-            <div className='calcTop' style={ width > 1199 ? {color: 'black'} : {color: 'grey'}}>48px</div>
-            <div className='calcMid'>{ calc }</div>
-            <div className='calcBot' style={ width < 481 ? {color: 'black'} : {color: 'grey'}}>24px</div>
+            <div className='calcTop' style={ width > 1199 ? {color: 'black', fontWeight: 'bold'} : {color: 'grey'}}>48px</div>
+            <div className='calcMid' style={ 480 < width && width < 1200 ? {color: 'black', fontWeight: 'bold'} : {color: 'grey'}}>{ calc }</div>
+            <div className='calcBot' style={ width < 481 ? {color: 'black', fontWeight: 'bold'} : {color: 'grey'}}>24px</div>
           </div>
         </div>
         <WindowResizeListener onResize={windowSize => {
